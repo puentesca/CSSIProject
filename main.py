64d012431323854aadfdf257e76153698db8c866
Casey Puentes <puentesc121@gmail.com>
2019-07-23 11:57:47 -0700

    Added handlers for pages

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import webapp2
 import jinja2
 import os
+from google.appengine.ext import ndb
+from data-classes import SubmissionRecord, SubmissionDatabase, UserAccount
 
 the_jinja_env = jinja2.Environment(
     loader = jinja2.FileSystemLoader(os.path.dirname(__file__)),
@@ -32,6 +34,7 @@ class DepartmentInfoPage(webapp2.RequestHandler):
         welcome_template = the_jinja_env.get_template('/templates/department-info.html')
         self.response.write(welcome_template.render())
     def post(self):
+        reportdescription = self.request.get("")
         pass
 
 # the handler section
@@ -40,6 +43,7 @@ class FormPage(webapp2.RequestHandler):
         welcome_template = the_jinja_env.get_template('/templates/form-page.html')
         self.response.write(welcome_template.render())
     def post(self):
+
         pass
 
 # the handler section
@@ -52,9 +56,11 @@ class SubmissionConfirmedPage(webapp2.RequestHandler):
 
 
 
-
-
 # the app configuration section
 app = webapp2.WSGIApplication([
     ('/', HomePage),
+    ('/tag-info', TagInfoPage),
+    ('/department-info', DepartmentInfoPage),
+    ('/report', FormPage),
+    ('/report-confirmed', SubmissionConfirmedPage)
 ], debug = True)

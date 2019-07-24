3b58b7b656027cdd2cad194aefc5eb9d40d643c6
Seema Kulkarni <seemariva@gmail.com>
2019-07-24 14:00:08 -0700

    updated location for API

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 import webapp2
 import jinja2
 import os
+import json
 from google.appengine.ext import ndb
+from google.appengine.api import urlfetch
 from data_classes import SubmissionRecord, SubmissionDatabase#, UserAccount
-
+api2key="AIzaSyAxRqWmRH0WoaqkSYbLOMIg3roBnPJTqFo"
 the_jinja_env = jinja2.Environment(
     loader = jinja2.FileSystemLoader(os.path.dirname(__file__)),
     extensions = ['jinja2.ext.autoescape'],
@@ -40,13 +42,36 @@ class DepartmentInfoPage(webapp2.RequestHandler):
 class FormPage(webapp2.RequestHandler):
     def get(self):
         welcome_template = the_jinja_env.get_template('/templates/form-page.html')
-        self.response.write(welcome_template.render())
+
+        url = "https://www.googleapis.com/geolocation/v1/geolocate?key=" + api2key
+        api_location = urlfetch.fetch( url, method="POST")
+        json_data = json.loads(api_location.content)
+        lat = json_data["location"]["lat"]
+        lng = json_data["location"]["lng"]
+        url2 = "https://maps.googleapis.com/maps/api/geocode/json?latlng="+str(lat)+","+str(lng)+"&key=" + api2key
+        api_location = urlfetch.fetch(url2, method="POST")
+        json_data = json.loads(api_location.content)
+        location_addresses = json_data["results"]
+        formatted_addresses = []
+        for address in location_addresses:
+            formatted_addresses.append(address["formatted_address"])
+        print("formatted_addresses")
+        # location_suggestions = json_data[results][0, 15]["formatted_address"]
+        template_values =  {
+        "addr": formatted_addresses,
+        # "suggestions": location_suggestions
+        }
+
+
+        self.response.write(welcome_template.render(template_values))
     def post(self):
+
         report_username = "test username" #self.request.get("username")
         report_email = "test email" #self.request.get("email")
         report_image_url = self.request.get("image-url")
         report_description = self.request.get("description")
         report_location = self.request.get("location")
+        report_location = self.request.get("location suggestions")
         report_tags = self.request.get("tags")
         report_urgency = self.request.get("urgency-level")
         record = SubmissionRecord(username = report_username, email = report_email, image_url = report_image_url, description = report_description, location = report_location, tags = report_tags, urgency = report_urgency)
